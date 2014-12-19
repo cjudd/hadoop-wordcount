0658de295e51057014ff67af1431de94b9c9c3c6
Christopher M. Judd <javajudd@gmail.com>
2014-12-18 20:57:26 -0500

    Changed type of incoming key to be a Hadoop type.

--- src/main/java/com/manifestcorp/hadoop/wc/WordCountMapper.java
+++ src/main/java/com/manifestcorp/hadoop/wc/WordCountMapper.java
@@ -3,17 +3,18 @@ package com.manifestcorp.hadoop.wc;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> { 
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 	private static final String SPACE = " ";
 	
 	private static final IntWritable ONE = new IntWritable(1);
 	private Text word = new Text();
 	
-	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		String[] words = value.toString().split(SPACE);
 		
 		for (String str: words) {
