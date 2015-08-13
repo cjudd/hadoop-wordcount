f845fc0e37406027d0e7fad9e9c37d3fb8f8b364
Christopher M. Judd <javajudd@gmail.com>
2015-08-13 07:28:21 -0400

    Tested using he SONAR //NOSONAR feature to suppress sonar validation rules.

--- src/main/java/com/manifestcorp/hadoop/wc/WordCountReducer.java
+++ src/main/java/com/manifestcorp/hadoop/wc/WordCountReducer.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 	
-	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {  //NOSONAR
 		int total = 0;
 		
 		for (IntWritable value : values) {
