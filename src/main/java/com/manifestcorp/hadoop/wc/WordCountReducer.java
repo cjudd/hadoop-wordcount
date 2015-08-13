dfd80253f879e70bc00d14010fa19851a86ae5ef
Christopher M. Judd <javajudd@gmail.com>
2015-08-13 08:21:02 -0400

    Revert "Tested using he SONAR //NOSONAR feature to suppress sonar validation rules."
    This reverts commit f845fc0e37406027d0e7fad9e9c37d3fb8f8b364.

--- src/main/java/com/manifestcorp/hadoop/wc/WordCountReducer.java
+++ src/main/java/com/manifestcorp/hadoop/wc/WordCountReducer.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 	
-	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {  //NOSONAR
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 		int total = 0;
 		
 		for (IntWritable value : values) {
